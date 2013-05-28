5bf070ad3cf73ba41de2bc736a56874bfe4fb6eb
evert <evert.lammerts@gmail.com>
2013-05-28 10:53:30 +0200

    cleaned up code

--- processors/learning.py
+++ processors/learning.py
@@ -1,14 +1,15 @@
 from core import LinksProcessor
 
+
 class LearningProcessor(LinksProcessor):
     def __init__(self, scikit_url=None):
         self.scikit_url = scikit_url
-        if scikit_url == None:
-            import scikit_light
 
     def process(self, links, text, settings):
         if "learning" in settings:
-            links = self.apply_learning(settings["learning"], links, settings.has_key("features"))
+            links = self.apply_learning(settings["learning"],
+                                        links,
+                                        "features" in settings)
         return (links, text, settings)
 
     def apply_learning(self, model, links, keep_features):
@@ -16,8 +17,8 @@ class LearningProcessor(LinksProcessor):
             return links
 
         features = sorted(links[0]["features"].keys())
-        
-        if self.scikit_url == None:            
+
+        if self.scikit_url == None:
             import scikit_light
 
             testfeatures = []
@@ -26,7 +27,7 @@ class LearningProcessor(LinksProcessor):
                 for feature in features:
                     linkfeatures.append(link["features"][feature])
                 testfeatures.append(linkfeatures)
-                
+
             scores = scikit_light.predict(model, testfeatures)
             for link, score in zip(links, scores):
                 link["learning_probability"] = score[1]
@@ -40,15 +41,16 @@ class LearningProcessor(LinksProcessor):
                 for index, feature in enumerate(features):
                     data += " %d:%f" % (index, link["features"][feature])
                 data += "\n"
-                
-            import urllib, urllib2
+
+            import urllib2
             url = self.scikit_url + "/predict/" + model
             request = urllib2.urlopen(urllib2.Request(url, data, {"Content-Type": "text/plain"}))
             scores = request.read().split('\n')
             for link, score in zip(links, scores):
-                if len(score) == 0 or " " not in score: score = "0 0"
+                if len(score) == 0 or " " not in score:
+                    score = "0 0"
                 link["learning_probability"] = float(score.split()[1])
                 if not keep_features:
                     del link["features"]
-        
+
         return links
--- processors/scikit_light.py
+++ processors/scikit_light.py
@@ -1,18 +1,22 @@
 from sklearn.externals import joblib
 
 pickle_cache = {}
+
+
 def pickle_load(filename):
-    if pickle_cache.has_key(filename):
+    if filename in pickle_cache:
         return pickle_cache[filename]
-    object = joblib.load(filename)
-    pickle_cache[filename] = object
-    return object
+    obj = joblib.load(filename)
+    pickle_cache[filename] = obj
+    return obj
+
 
 def predict(classifier, testfeatures):
     clf = pickle_load("pickles/" + classifier)
     print("Loaded classifier pickle from %s" % classifier)
-        
-    print("Start predicting of %d instances with %d features." % (len(testfeatures), len(testfeatures[0])))
+
+    print("Start predicting of %d instances with %d features."
+          % (len(testfeatures), len(testfeatures[0])))
     predict = clf.predict_proba(testfeatures)
     print("Done predicting of %d instances." % len(predict))
 
