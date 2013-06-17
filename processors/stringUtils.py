e6a60748eb7a7ba743584672352a8cdd137134ab
Daan Odijk <d.odijk@uva.nl>
2013-06-17 11:39:51 +0200

    Fixed config file and UTF-8.

--- processors/stringUtils.py
+++ processors/stringUtils.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#
 # Copyright 2012-2013, University of Amsterdam. This program is free software:
 # you can redistribute it and/or modify it under the terms of the GNU Lesser 
 # General Public License as published by the Free Software Foundation, either 
@@ -10,8 +12,6 @@
 # 
 # You should have received a copy of the GNU Lesser General Public License 
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# -*- coding: utf-8 -*-
 
 import re
 
