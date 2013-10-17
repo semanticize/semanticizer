bae7e18180f1e31cbc832ed56de8a771c5bf6725
Lars Buitinck <l.buitinck@esciencecenter.nl>
2013-10-17 16:44:31 +0200

    shebang on semanticizer_wsgi.py

--- semanticizer_wsgi.py
+++ semanticizer_wsgi.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright 2012-2013, University of Amsterdam. This program is free software:
 # you can redistribute it and/or modify it under the terms of the GNU Lesser 
 # General Public License as published by the Free Software Foundation, either 
